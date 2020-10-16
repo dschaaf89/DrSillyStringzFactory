e85d499ae932250253aecd95b5ad1632aa2fe08a
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-16 08:45:43 -0700

    added Machine Properties

--- DrSillyStringzFactory/Models/Engineer.cs
+++ DrSillyStringzFactory/Models/Engineer.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+
 namespace DrSillyStringzFactory.Models
 {
     public class Engineer
     {
-        
+        public Engineer()
+        {
+            this.EngineerMachine = new HashSet<EngineerMachine>();
+        }
+        public int EngineerId { get; set; } 
+        public string Name { get; set; }
+        public string License { get; set; }
+
+        public virtual ICollection<EngineerMachine> EngineerMachine {get;set;}
+
     }
 }
--- DrSillyStringzFactory/Models/Machine.cs
+++ DrSillyStringzFactory/Models/Machine.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
 namespace DrSillyStringzFactory.Models
 {
-    public class Machine
+    public class Machine 
     {
+     public Machine()
+        {
+            this.EngineerMachine = new HashSet<EngineerMachine>();            
+        }
+        public int MachineId {get; set;} 
+        public string Name {get;set;}
+        public virtual ICollection<EngineerMachine> EngineerMachine {get; set;}
         
-    }
+        
+}
 }
